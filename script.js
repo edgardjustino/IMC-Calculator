2e148978cd74b6723acdb973c76d54491b24b399
edgardjustino <eddiejustino@gmail.com>
2023-03-05 22:21:19 -0300

    acertos CSS e começo do JS

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("teste")
+const height = document.querySelector('#height')
+const weight = document.querySelector('#weight')
+const calculate = document.querySelector('')
+const clear = document.querySelector('')
+
