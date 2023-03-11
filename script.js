01b3abe29b72f3c60731bbcc0126103098c92bed
edgardjustino <eddiejustino@gmail.com>
2023-03-10 22:26:31 -0300

    ajustando js

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const calculate = document.querySelector('#calc-btn');
+// const calculate = document.querySelector('#calc-btn');
 const clear = document.querySelector('#clear-btn');
 const result = document.querySelector('#result');
-const height = document.querySelector('#height').value;
+const height = (document.querySelector('#height').value)/100;
 const weight = document.querySelector('#weight').value;
 
 // dividir o peso pela altura ao quadrado
 
 function clearInputs (){
-height = "";
-weight = "";
-}
+    clear.addEventListener('click', ()=>{
+        height = '';
+        weight = '';
+    } )
+};
 
 function calculateImc () {
+    // const height = document.querySelector('#height').value;
+    // const weight = document.querySelector('#weight').value;
+
+    const  imc = weight / (height * height);
+    const text = '';
+
+    if (imc < 18.5){
+        result.innerHTML = `Seu IMC é ${imc} e você está abaixo do peso ideal`
+    } else if (imc < 24.9){
+        result.innerHTML = `Seu IMC é ${imc} e você está com o peso ideal`
+    } else if (imc<29.9){
+        result.innerHTML = `Seu IMC é ${imc} e você está com sobrepeso`
+    } else if (imc < 39.9){
+        result.innerHTML = `Seu IMC é ${imc} e você está abaixo do peso ideal`
+    } else if ( imc > 39.9){
+        result.innerHTML = `Seu IMC é ${imc} e você está com obesidade mórbida`
+    }
     
-    height / (weight*weight);
+};
 
-    calculate.addEventListener('click', calculateImc);
-}
-
-clear.addEventListener('click', (clearInputs) => {
-    clear.preventDefault();
+// fist attempt
     clearInputs();
-});
