90954b51d8f192178a1146e115ba379ede805949
edgardjustino <eddiejustino@gmail.com>
2023-03-15 17:00:06 -0300

    input limpar funcionando

--- script.js
+++ script.js
@@ -5,33 +5,40 @@ const calculate = document.querySelector('#calc-btn');
 // const result = document.querySelector('#result');
 
 function imc () {
-    
-    if (height !== '' && weight !== ''){
+
         const height = (document.querySelector('#height').value)/100;
         const weight = document.querySelector('#weight').value;
         const result = document.querySelector('#result');
-    
-        const valorIMC = (weight / (height * height)).toFixed(1);
 
-        let classificacao = '';
+        const valorIMC = (weight / (height * height)).toFixed(1);
+    
+    if (height !== '' && weight !== ''){
+    
+    let classificacao = '';
 
         if (valorIMC <18.5){
-            classificacao = 'abaixo do peso';
+            classificacao = 'abaixo do peso.';
         } else if (valorIMC <25) {
             classificacao = 'com peso ideal! Parabéns!';
-        } else if (valorIMC<30){
-            classificacao = 'levemente acima do peso';
+        } else if (valorIMC<30) {
+            classificacao = 'levemente acima do peso.';
         } else if ( valorIMC < 35) {
             classificacao = 'com obesidade grau I.';
         } else if (valorIMC < 40) {
-            classificacao = 'com obesidade grau II';
+            classificacao = 'com obesidade grau II.';
         } else {
             classificacao = 'com obesidade grau III. Cuidado!';
         }
         result.textContent = `Seu IMC é ${valorIMC} e você está ${classificacao}`;
     } else {
-        result.textContent = 'Preencha todos os campos.'; // why this result isn't a const??
+        result.textContent = 'Por favor, preencha todos os campos.';
     }
+}
+
+calculate.addEventListener('click', imc);
+
+function reload(){
+   window.location.reload()
 }
 
 
@@ -45,7 +52,4 @@ function imc () {
 //     height = '';
 // }); 
 
-calculate.addEventListener('click', imc);
-
-//fazer o botão clear funcionar/make the clear button work
 //dizer que tem um campo vazio
